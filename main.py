1d3833f9ce275253786c5632db3522f93458fb4c
yjr2003 <143598780+yjr2003@users.noreply.github.com>
2023-10-18 18:31:32 +0900

    initial version of main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from typing import List
 
 def even_list(int_list: List[int]) -> List[int]:
-    return [num for num in int_list if num % 2 == 0]
+    """Determines if a number is even and returns an even list.
+
+    Args:
+        int_list: A list of integers.
+
+    Returns:
+        A list of even integers.
+    """
+    # TODO: Implement even_list    
+    pass
 
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    """
+    Computes the sum of the squares of all even numbers in a list of integers.
+
+    Args:
+        even_int_list: A list of even integers.
+
+    Returns:
+        The sum of the squares of all even numbers in the list.
+    """
     return sum(num**2 for num in even_int_list)
 
 def main():
